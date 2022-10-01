b53b790187be4f462d792d978ca25ac341f43317
Safar Abbas <97079688+Safarababs@users.noreply.github.com>
2022-10-01 23:33:18 +0300

    Update footer.jsx

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     const date = new Date().getFullYear();
     return (
     <footer className="w3-container w3-center w3-margin-top">
-        <p className="w3-text-white">Safar abbas © copyright {date} all rights reserverd</p>
+        <p className="w3-text-white">Safar abbas © copyright @ {date} | all rights reserverd</p>
     </footer>
     )
 }
